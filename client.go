5f984c22fc3a13825681f03ae44a8cbb096754b2
emersion <contact@emersion.fr>
2016-12-27 19:22:48 +0100

    Improves docs, changes API

--- client.go
+++ client.go
@@ -8,27 +8,41 @@ import (
 	imapclient "github.com/emersion/go-imap/client"
 )
 
+// ErrAlreadyEnabled is returned by Client.Compress when compression has
+// already been enabled on the client.
+var ErrAlreadyEnabled = errors.New("COMPRESS is already enabled")
+
+// ErrNotSupported is returned by Client.Compress when the provided
+// compression mechanism is not supported.
+type ErrNotSupported struct {
+	Mechanism string
+}
+
+// Error implements error.
+func (err ErrNotSupported) Error() string {
+	return "Cannot start compression: mechanism " + err.Mechanism + " not supported"
+}
+
+// Client is a COMPRESS client.
 type Client struct {
 	client *imapclient.Client
 	isCompressed bool
 }
 
-// Instructs the server to use the named compression mechanism for all commands
-// and/or responses.
-func (c *Client) Compress(mech string) (err error) {
+// Compress instructs the server to use the named compression mechanism for all
+// commands and/or responses.
+func (c *Client) Compress(mech string) error {
 	if c.isCompressed {
-		err = errors.New("COMPRESS is already enabled")
-		return
+		return ErrAlreadyEnabled
 	}
 
 	if mech != Deflate {
-		err = errors.New("Cannot start compression: mechanism " + mech + " not supported")
-		return
+		return ErrNotSupported{mech}
 	}
 
 	cmd := &Command{Mechanism: mech}
 
-	err = c.client.Upgrade(func (conn net.Conn) (net.Conn, error) {
+	err := c.client.Upgrade(func (conn net.Conn) (net.Conn, error) {
 		if status, err := c.client.Execute(cmd, nil); err != nil {
 			return nil, err
 		} else if err := status.Err(); err != nil {
@@ -38,24 +52,24 @@ func (c *Client) Compress(mech string) (err error) {
 		return NewDeflateConn(conn, flate.DefaultCompression)
 	})
 	if err != nil {
-		return
+		return err
 	}
 
 	c.isCompressed = true
-	return
+	return nil
 }
 
-// Check if this client's connection is compressed.
-func (c *Client) IsCompressed() bool {
+// IsCompressed checks if this client's connection is compressed.
+func (c *Client) IsCompress() bool {
 	return c.isCompressed
 }
 
-// Check if the server supports a compression mechanism.
+// SupportCompress checks if the server supports a compression mechanism.
 func (c *Client) SupportCompress(mech string) (bool, error) {
 	return c.client.Support(Capability + "=" + mech)
 }
 
-// Create a new client.
+// NewClient creates a new client.
 func NewClient(c *imapclient.Client) *Client {
 	return &Client{client: c}
 }
